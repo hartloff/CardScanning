873a43526aeda808e183a6f688e41d698e588abb
SpookyDev <iamaspookydev@gmail.com>
2017-07-03 21:26:58 -0400

    rename ids

--- views/private/scripts.js
+++ views/private/scripts.js
@@ -9,41 +9,43 @@ TODO: re-design form validation method
 $(document).ready(function() {
 
 	// person # field validation
-	$('#person-number').focusout(function() {
+	$('#personNum').focusout(function() {
 		var value = $(this).val();
 
-		$('#person-number-msg').html(
+		$('#personNum-msg').html(
 			(value === '' && 'you must provide your person #') ||
 			((value.length !== 8 || isNaN(value)) && 'person # must be an 8-digit number') || ''
 		);
 	});
 
 	// question type field validation
-	$('#question-type').focusout(function() {
-		$('#question-type-msg').html(
-			($('#question-type').val().length === 0 && 'you must select at least one item') || ''
+	$('#qType').focusout(function() {
+		$('#qType-msg').html(
+			($('#qType').val().length === 0 && 'you must select at least one item') || ''
 		);
 	});
 
 	// option 'other' event listener
-	$('#question-type').change(function() {
+	$('#qType').change(function() {
 		if($('option[value="Other"]').is(':selected')) {
-			$('#question-type-other-field').html(
-				'<label class="form-control-label" for="question-description">Question description:</label>' +
-				'<input class="form-control" id="question-description" name="question-description" placeholder="Brief description" type="text" required>' +
-				'<p class="form-text" id="question-description-msg"></p>'
+			$('#qDescField').html(
+				'<label class="form-control-label" for="qDesc">Question description:</label>' +
+				'<input class="form-control" id="qDesc" name="question-description" placeholder="Brief description" type="text" required>' +
+				'<p class="form-text" id="qDesc-msg"></p>'
 			);
 
 			// question description field validation
-			$('#question-description').focusout(function() {
-				$('#question-description-msg').html(
-					($('#question-description').val() === '' && 'you must provide a question description') || ''
+			$('#qDesc').focusout(function() {
+				$('#qDesc-msg').html(
+					($('#qDesc').val() === '' && 'you must provide a question description') || ''
 				);
 			});
 		} else {
+			
 			// detach event handler
-			$('#question-description').off('focusout');
-			$('#question-type-other-field').html('');
+			$('#qDesc').off('focusout');
+
+			$('#qDescField').html('');
 		}
 	});
 
