34ce95afbe308fdb968eec6eebadf30ce0b90fb8
SpookyDev <iamaspookydev@gmail.com>
2017-07-07 13:34:03 -0400

    update validation method

--- views/private/formValidate.js
+++ views/private/formValidate.js
@@ -8,48 +8,29 @@ TODO: question type validation
 
 $(document).ready(function() {
 
-	// field ready state
-	var personNumReady = false; // person # ready state
-	var datetimeReady = false; // datetime ready state
-
-	// event 'input:change' emitter for all input fields
-	$('input').keyup(function(event) {
-		$(event.target).trigger('input:change');
-	});
-
-	// person # field validation
-	$('#personNum').on('input:change focusout form:error', function() {
-		var value = $(this).val();
-
-		var isEmpty = value === '';
-		var isInvalid = value.length !== 8 || isNaN(value);
-		personNumReady = !(isEmpty || isInvalid);
+	$('form').submit(function() {
+		var personNumReady = /^[0-9]{8}$/.test($('#personNum').val());
+		var qTypeReady = $('#qType').val().length !== 0;
+		var qDescriptionReady = $('#qDescription').val().trim().length !== 0;
+		var datetimeReady = $('#datetime').val().length !== 0;
 
 		$('#personNum-msg').html(
-			(isEmpty && 'you must provide your person #') ||
-			(isInvalid && 'person # must be an 8-digit number') || ''
+			(!personNumReady && 'person # must be an 8-digit number') || ''
 		);
-	});
 
-	// TODO: question type
+		$('#qType-msg').html(
+			(!qTypeReady && 'you must select at least one item') || ''
+		);
 
-	// datetime field validation
-	$('#datetime').on('input:change focusout form:error' ,function() {
-		datetimeReady = !($(this).val() === '');
+		$('#qDescription-msg').html(
+			(!qDescriptionReady && 'you must provide a brief description') || ''
+		);
 
 		$('#datetime-msg').html(
 			(!datetimeReady && 'you must provide a date and time') || ''
 		);
-	});
 
-	// submit handler
-	$('form').submit(function() {
-		if(!(personNumReady && datetimeReady)) {
-			$('input').trigger('form:error');
-			return false;
-		}
-
-		return true;
+		return personNumReady && qTypeReady && qDescriptionReady && datetimeReady;
 	});
 
 });
